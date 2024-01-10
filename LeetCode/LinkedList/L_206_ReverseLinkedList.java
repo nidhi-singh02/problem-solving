16a0784f202ea892ab343c65dda782b00b2da6d3
nidhi-singh02 <nidhi2894@gmail.com>
2024-01-10 22:57:45 +0530

    L_206_ReverseLinkedList
    Signed-off-by: nidhi-singh02 <nidhi2894@gmail.com>

--- LeetCode/LinkedList/L_206_ReverseLinkedList.java
+++ LeetCode/LinkedList/L_206_ReverseLinkedList.java
@@ -1,5 +1,30 @@
 package LeetCode.LinkedList;
 
 public class L_206_ReverseLinkedList {
+    public static class ListNode {
+        int val;
+        L_206_ReverseLinkedList.ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, L_206_ReverseLinkedList.ListNode next) { this.val = val; this.next = next; }
+    }
 
-}
+        public ListNode reverseList(ListNode head) {
+            // Follow iterative approach using two-pointer mechanism
+            // Two pointers = current pointer starts at head and
+            // prev pointer will be null as there is no element before head
+
+            ListNode current = head;
+            ListNode prev = null;
+            while (current != null){
+                ListNode next = current.next;
+                current.next = prev;
+                // next.next = current;
+                prev = current;
+                current = next;
+            }
+
+            return prev;
+        }
+    }
+
