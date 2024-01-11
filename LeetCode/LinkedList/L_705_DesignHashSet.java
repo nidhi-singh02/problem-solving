7f5f5972e6fecc6fff4ffa44a56dd7748f9f3a98
nidhi-singh02 <nidhi2894@gmail.com>
2024-01-11 20:12:03 +0530

    DesignHashSet
    Signed-off-by: nidhi-singh02 <nidhi2894@gmail.com>

--- LeetCode/LinkedList/L_705_DesignHashSet.java
+++ LeetCode/LinkedList/L_705_DesignHashSet.java
@@ -7,27 +7,52 @@ class MyHashSet {
     // HashSet will not have duplicate value
 
     // Approach 1: Use ArrayList for designing HashSet
-    List<Integer> list;
+    // List<Integer> list;
+    // // List can have duplicate, so need to have checks in add function
+
+    // public MyHashSet() {
+    //     list = new ArrayList<>();
+    // }
+
+    // public void add(int key) {
+    //     if(!contains(key)){
+    //         list.add(key);
+    //     }
+    // }
+
+    // public void remove(int key) {
+    //         if(contains(key)){
+    //             int KeyIndex = list.indexOf(key); // If key doesn not in the list, indexOf will return -1
+    //             list.remove(KeyIndex); // remove takes index
+    //         }
+    // }
+
+    // // O(n)
+    // public boolean contains(int key) {
+    //     return list.contains(key);
+    // }
+
+    List<Boolean> list;
 
     public MyHashSet() {
         list = new ArrayList<>();
+        for(int i=0;i <= 1000000;i++){
+            list.add(false);
+        }
     }
 
     public void add(int key) {
-        if(!contains(key)){
-            list.add(key);
-        }
+        list.set(key,true);// add will not replace the element, it will return false if it occurs
     }
 
     public void remove(int key) {
-        if(contains(key)){
-            int KeyIndex = list.indexOf(key);
-            list.remove(KeyIndex); // remove takes index
-        }
+        // list.remove(key); // remove will move subsequent elements to left
+        list.set(key,false);
     }
 
-    public boolean contains(int key) {
-        return list.contains(key);
+    // Time complexity  O(1)
+    public boolean contains(int key){
+        return list.get(key); // get works on index
     }
 }
 
