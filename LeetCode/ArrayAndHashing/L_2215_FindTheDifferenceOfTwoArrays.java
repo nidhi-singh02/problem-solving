363b97f1b19bc1d320bc3108e9485dd12a802e84
nidhi-singh02 <nidhi2894@gmail.com>
2024-01-20 11:42:04 +0530

    L_2215_FindTheDifferenceOfTwoArrays - use HashSet
    Signed-off-by: nidhi-singh02 <nidhi2894@gmail.com>

--- LeetCode/ArrayAndHashing/L_2215_FindTheDifferenceOfTwoArrays.java
+++ LeetCode/ArrayAndHashing/L_2215_FindTheDifferenceOfTwoArrays.java
@@ -1,9 +1,6 @@
 package LeetCode.ArrayAndHashing;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class L_2215_FindTheDifferenceOfTwoArrays {
@@ -13,27 +10,49 @@ public class L_2215_FindTheDifferenceOfTwoArrays {
 
     public static List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         // Use HashMap with key as number and value as occurring in which array
+//
+//        Map<Integer,Integer> hashMap = new HashMap<>();
+//
+//        for(int num1: nums1){
+//            hashMap.put(num1,0); // if duplicate key found it will override the value
+//        }
+//
+//        for(int num2:nums2){
+//            if(!hashMap.containsKey(num2)){
+//                hashMap.put(num2,1);
+//            }else if(hashMap.containsKey(num2) && hashMap.get(num2) == 0){
+//                hashMap.put(num2,99);
+//            }
+//        }
+//
+//        System.out.println("map: "+hashMap);
+//        List<List<Integer>> answer = new ArrayList<>();
+//
+//        answer.add(hashMap.entrySet().stream().filter(e -> e.getValue().equals(0)).map( Map.Entry::getKey).collect(Collectors.toList()));
+//        answer.add(hashMap.entrySet().stream().filter(e -> e.getValue().equals(1)).map( Map.Entry::getKey).collect(Collectors.toList()));
+//
+//        return  answer;
 
-        Map<Integer,Integer> hashMap = new HashMap<>();
+       return Arrays.asList(getElementsOnlyInOneList(nums1,nums2),
+        getElementsOnlyInOneList(nums2,nums1));
 
-        for(int num1: nums1){
-            hashMap.put(num1,0); // if duplicate key found it will override the value
+    }
+
+    private static List<Integer> getElementsOnlyInOneList(int[] nums1, int[] nums2){
+        Set<Integer> numberOnlyIn1 = new HashSet<>();
+        Set<Integer> existsInNums2 = new HashSet<>();
+
+        for(int n2: nums2){
+            existsInNums2.add(n2);
         }
 
-        for(int num2:nums2){
-            if(!hashMap.containsKey(num2)){
-                hashMap.put(num2,1);
-            }else if(hashMap.containsKey(num2) && hashMap.get(num2) == 0){
-                hashMap.put(num2,99);
+        for(int n1: nums1){
+            if (!existsInNums2.contains(n1)){
+                numberOnlyIn1.add(n1);
             }
         }
-
-        System.out.println("map: "+hashMap);
-        List<List<Integer>> answer = new ArrayList<>();
-
-        answer.add(hashMap.entrySet().stream().filter(e -> e.getValue().equals(0)).map( Map.Entry::getKey).collect(Collectors.toList()));
-        answer.add(hashMap.entrySet().stream().filter(e -> e.getValue().equals(1)).map( Map.Entry::getKey).collect(Collectors.toList()));
-
-        return  answer;
+       return new ArrayList<>(numberOnlyIn1);
     }
+
+
 }
