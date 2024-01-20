3437f91768daae74c110bcfc3abfc4a18e4bd91e
nidhi-singh02 <nidhi2894@gmail.com>
2024-01-20 11:14:01 +0530

    L_1732_FindTheHighestAltitude- another approach
    Signed-off-by: nidhi-singh02 <nidhi2894@gmail.com>

--- LeetCode/ArrayAndHashing/L_1732_FindTheHighestAltitude.java
+++ LeetCode/ArrayAndHashing/L_1732_FindTheHighestAltitude.java
@@ -2,7 +2,7 @@ package LeetCode.ArrayAndHashing;
 
 public class L_1732_FindTheHighestAltitude {
     public static void main(String[] args){
-        System.out.println("result: "+largestAltitude(new int[]{-4,-3,-2,-1,4,3,2}));
+        System.out.println("result: "+largestAltitude(new int[]{-5,1,5,0,-7}));
     }
     public static int largestAltitude(int[] gain) {
         // Traverse the array, create new array for altitudes
@@ -24,27 +24,37 @@ public class L_1732_FindTheHighestAltitude {
 //        }
 //        return largestAlt;
 
-        // Second approach using Hint
-        int[] altitudes = new int[gain.length+1];
-        altitudes[0] = 0;
-        int sum = 0;
-        for(int i=0;i < gain.length;i++){
-          sum = sum + gain[i];
-        }
+//        // Second approach using Hint- altitude of an element is the sum of gains of all the elements behind it
+//        int[] altitudes = new int[gain.length+1];
+//        altitudes[0] = 0;
+//        int sum = 0;
+//        for(int i=0;i < gain.length;i++){
+//          sum = sum + gain[i];
+//        }
+//
+//        altitudes[gain.length] = sum;
+//        for(int j=gain.length-1; j > 0; j--){
+//            altitudes[j] = sum - gain[j];
+//            sum = sum - gain[j];
+//        }
+//        altitudes[0] = 0;
+//        int largestAlt = Integer.MIN_VALUE;
+//        for(int al: altitudes){
+//            if(al > largestAlt){
+//                largestAlt = al;
+//            }
+//        }
+//        return largestAlt;
 
-        altitudes[gain.length] = sum;
-        for(int j=gain.length-1; j > 0; j--){
-            altitudes[j] = sum - gain[j];
-            sum = sum - gain[j];
-        }
-        altitudes[0] = 0;
-        int largestAlt = Integer.MIN_VALUE;
-        for(int al: altitudes){
-            if(al > largestAlt){
-                largestAlt = al;
-            }
-        }
-        return largestAlt;
 
+        // Third approach - Prefix Sum
+        int currentAltitudes = 0;
+        int largestAlt = currentAltitudes;
+         for (int i = 0; i < gain.length; i++) {
+             currentAltitudes =  currentAltitudes + gain[i];
+             largestAlt = Math.max(largestAlt,currentAltitudes);
+         }
+
+         return largestAlt;
     }
 }
