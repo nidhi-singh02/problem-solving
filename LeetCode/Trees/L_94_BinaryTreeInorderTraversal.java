550d5d81ddf259e51a912bbc4a815cfed8832343
nidhi-singh02 <nidhi2894@gmail.com>
2024-01-23 10:31:21 +0530

    L_94_BinaryTreeInorderTraversal - iterative
    Signed-off-by: nidhi-singh02 <nidhi2894@gmail.com>

--- LeetCode/Trees/L_94_BinaryTreeInorderTraversal.java
+++ LeetCode/Trees/L_94_BinaryTreeInorderTraversal.java
@@ -11,17 +11,34 @@ public class L_94_BinaryTreeInorderTraversal {
     }
 
     public static List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> result = new ArrayList<>();
+//        inorder(root,result);
+//        return result;
+
+        // Second approach - Iterative
         List<Integer> result = new ArrayList<>();
-        inorder(root,result);
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while(curr != null || !stack.isEmpty()){
+            while( curr != null){
+                stack.push(curr);
+                curr = curr.left ;
+            }
+            curr = stack.pop();
+            result.add(curr.val);
+            curr = curr.right;
+        }
         return result;
+
     }
 
-   private static void inorder(TreeNode root,List<Integer> result){
-        if(root == null){
-            return;
-        }
-       inorder(root.left,result);
-        result.add(root.val);
-       inorder(root.right,result);
-    }
+   private static void inorder(TreeNode root,List<Integer> result) {
+       if (root == null) {
+           return;
+       }
+       inorder(root.left, result);
+       result.add(root.val);
+       inorder(root.right, result);
+   }
+
 }
