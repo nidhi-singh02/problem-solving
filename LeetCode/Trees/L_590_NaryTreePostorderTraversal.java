bffb31e1787beb6ea4d9063ceaecd2fcefa5e833
nidhi-singh02 <nidhi2894@gmail.com>
2024-01-23 19:38:52 +0530

    L_590_NaryTreePostorderTraversal - iterative
    Signed-off-by: nidhi-singh02 <nidhi2894@gmail.com>

--- LeetCode/Trees/L_590_NaryTreePostorderTraversal.java
+++ LeetCode/Trees/L_590_NaryTreePostorderTraversal.java
@@ -1,8 +1,11 @@
 package LeetCode.Trees;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
+//Recursion : To be revisited
 public class L_590_NaryTreePostorderTraversal {
    static class Node {
         public int val;
@@ -26,19 +29,41 @@ public class L_590_NaryTreePostorderTraversal {
 
     }
     public static List<Integer> postorder(Node root) {
+//        List<Integer> result = new ArrayList<>();
+//        post(root, result);
+//        return result;
+        //Second approach : Iterative
+        // Recursive approach
+        Stack<Node> stack = new Stack<>();
         List<Integer> result = new ArrayList<>();
-        post(root, result);
+        Node curr = root;
+
+        if(curr == null){
+            return result;
+        }
+        stack.add(root);
+
+        while (!stack.isEmpty()) {
+            curr = stack.pop();
+            result.addFirst(curr.val);
+
+            for (Node child : curr.children) {
+                stack.add(child);
+            }
+        }
+
+//        Collections.reverse(result);
         return result;
     }
 
-    private static void post(Node root, List<Integer> result) {
-        if (root == null) {
-            return;
-        }
-        for (Node child : root.children) {
-            post(child, result);
-        }
-
-        result.add(root.val);
-    }
+//    private static void post(Node root, List<Integer> result) {
+//        if (root == null) {
+//            return;
+//        }
+//        for (Node child : root.children) {
+//            post(child, result);
+//        }
+//
+//        result.add(root.val);
+//    }
 }
