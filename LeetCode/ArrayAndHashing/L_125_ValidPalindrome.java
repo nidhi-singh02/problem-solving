a098244d4a56445aaa1b9e5ec7cad4b9dd8eebb0
nidhi-singh02 <nidhi2894@gmail.com>
2024-02-10 22:22:40 +0530

    L_125_ValidPalindrome - second approach
    Signed-off-by: nidhi-singh02 <nidhi2894@gmail.com>

--- LeetCode/ArrayAndHashing/L_125_ValidPalindrome.java
+++ LeetCode/ArrayAndHashing/L_125_ValidPalindrome.java
@@ -1,23 +1,36 @@
 package LeetCode.ArrayAndHashing;
 
 import java.util.Collections;
+import java.util.Objects;
 
 public class L_125_ValidPalindrome {
     public static void main(String[] args) {
-        System.out.println("is palindrome :"+isPalindrome("A man, a plan, a canal -- Panama"));
+        System.out.println("is palindrome :" + isPalindrome("race a car"));
     }
+
     public static boolean isPalindrome(String s) {
-        String modifiedString = s.toLowerCase().strip().replaceAll("[\"-+:.\\-,\\[\\]@%\\\\$#!~^&*()_+=<>{}\\s]","");
-        System.out.println("modified: "+modifiedString);
-        int j=modifiedString.length()-1;
-        int i=0;
-       while(i < j){
-            if(modifiedString.charAt(i) != modifiedString.charAt(j)){
-                return false;
+//
+//        String modifiedString = s.toLowerCase().strip().replaceAll("[\"-+:.\\-,\\[\\]@%\\\\$#!~^&*()_+=<>{}\\s]","");
+//        System.out.println("modified: "+modifiedString);
+//        int j=modifiedString.length()-1;
+//        int i=0;
+//       while(i < j){
+//            if(modifiedString.charAt(i) != modifiedString.charAt(j)){
+//                return false;
+//            }
+//            i++;
+//            j--;
+//        }
+//        return true;
+        StringBuilder OnlyValid = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetterOrDigit(s.charAt(i))) {
+                OnlyValid.append(Character.toLowerCase(s.charAt(i)));
             }
-            i++;
-            j--;
         }
-        return true;
+        String filteredString = OnlyValid.toString();
+        String reversedString = OnlyValid.reverse().toString();
+        return filteredString.equals(reversedString);
+//       return Objects.equals(OnlyValid,OnlyValid.reverse());
     }
 }
