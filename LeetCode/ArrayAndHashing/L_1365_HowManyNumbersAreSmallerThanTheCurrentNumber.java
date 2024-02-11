bbc297d0ffe34c86d5e6574fb9c42a9c71f16c16
nidhi-singh02 <nidhi2894@gmail.com>
2024-02-11 23:34:32 +0530

    L_1365_HowManyNumbersAreSmallerThanTheCurrentNumber - second approach
    Signed-off-by: nidhi-singh02 <nidhi2894@gmail.com>

--- LeetCode/ArrayAndHashing/L_1365_HowManyNumbersAreSmallerThanTheCurrentNumber.java
+++ LeetCode/ArrayAndHashing/L_1365_HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -1,23 +1,42 @@
 package LeetCode.ArrayAndHashing;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class L_1365_HowManyNumbersAreSmallerThanTheCurrentNumber {
     public static void main(String[] args) {
         System.out.println("result: "+ Arrays.toString(smallerNumbersThanCurrent(new int[]{8,1,2,2,3})));
     }
     public static int[] smallerNumbersThanCurrent(int[] nums) {
+//        int[] result = new int[nums.length];
+//
+//        for(int i=0;i < nums.length;i++){
+//            int smallerNums = 0;
+//            for(int j=0; j < nums.length;j++){
+//                if(nums[i] > nums[j] && i != j){
+//                    smallerNums++;
+//                }
+//
+//            }
+//            result[i] =smallerNums;
+//        }
+//
+//        return result;
+
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
         int[] result = new int[nums.length];
-
-        for(int i=0;i < nums.length;i++){
-            int smallerNums = 0;
-            for(int j=0; j < nums.length;j++){
-                if(nums[i] > nums[j] && i != j){
-                    smallerNums++;
-                }
-
+        int i=0;
+        for(int num:nums){
+            List<Integer> allNums = map.entrySet().stream().filter(e -> e.getKey().compareTo(num) < 0).map(Map.Entry::getValue).collect(Collectors.toList());
+            int sum = 0;
+            for (int n : allNums) {
+                sum = sum + n;
             }
-            result[i] =smallerNums;
+            result[i] = sum;
+            i++;
         }
 
         return result;
